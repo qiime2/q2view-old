8bde8d8e0856e00d5d387445f76eefc3c7bf8121
Matthew Dillon <matthewrdillon@gmail.com>
2016-10-11 11:23:18 -0700

    SQUASH: Minor cleanup

--- app/js/main.js
+++ app/js/main.js
@@ -21,39 +21,43 @@ if ('serviceWorker' in navigator) {
 const logo = document.getElementById('logo');
 logo.src = image;
 
+function processOld(data) {
+    let dataObjects = Object.keys(data.files)
+        .filter(key => key.split('/')[1] === 'data')
+        .map(key => data.files[key]);
+    dataObjects = dataObjects.map((dataObject) => {
+        const obj = dataObject;
+        obj.name = `./${obj.name.split('/').slice(2).join('/')}`;
+        if (obj.name.endsWith('index.html')) {
+            obj.name = './visualization.html';
+        }
+        return obj;
+    });
+    new Promise((resolve, reject) => {
+        const messageChannel = new MessageChannel();
+        messageChannel.port1.onmessage = (event) => {
+            if (event.data.error) {
+                reject(event.data.error);
+            } else {
+                resolve(event.data);
+            }
+        };
+        dataObjects.forEach((dataObject) => {
+            dataObject.async('uint8array').then((arr) => {
+                const dict = {};
+                dict[dataObject.name] = arr;
+                navigator.serviceWorker.controller.postMessage(dict);
+            });
+        });
+    });
+}
+
 const fileinput = document.getElementById('fileinput');
 fileinput.addEventListener('change', (e) => {
     const zip = new JSZip();
     zip.loadAsync(e.target.files[0])
-    .then((data) => {
-        let dataObjects = Object.keys(data.files).filter(key => key.split('/')[1] === 'data').map(key => data.files[key]);
-        dataObjects = dataObjects.map((dataObject) => {
-            const obj = dataObject;
-            obj.name = `./${obj.name.split('/').slice(2).join('/')}`;
-            if (obj.name.endsWith('index.html')) {
-                obj.name = './visualization.html';
-            }
-            return obj;
-        });
-        new Promise((resolve, reject) => { // eslint-disable-line
-            const messageChannel = new MessageChannel();
-            messageChannel.port1.onmessage = (event) => {
-                if (event.data.error) {
-                    reject(event.data.error);
-                } else {
-                    resolve(event.data);
-                }
-            };
-            dataObjects.forEach((dataObject) => {
-                dataObject.async('uint8array').then((arr) => {
-                    const dict = {};
-                    dict[dataObject.name] = arr;
-                    navigator.serviceWorker.controller.postMessage(dict);
-                });
-            });
-        });
-    })
-    .then(() => {
+
+    .then(processOld).then(() => {
         setTimeout(() => { window.location.href = `${window.location.origin}/visualization.html`; }, 2000);
     });
 });
