a3091f45be31a48c61401c31eeeb44a3033c9514
Matthew Dillon <matthewrdillon@gmail.com>
2016-10-21 15:54:49 -0700

    ENH: Initial support for query-param QZV

--- app/js/util/sw.js
+++ app/js/util/sw.js
@@ -34,7 +34,6 @@ self.addEventListener('fetch', (fetchEvent) => {
     let filename = components.slice(1).join('/');  // everything but session
     let ext = filename.split('.').pop()
 
-
     fetchEvent.respondWith(new Promise((resolve, reject) => {
         let channel = new MessageChannel();
         channel.port1.onmessage = (event) => {
@@ -43,57 +42,8 @@ self.addEventListener('fetch', (fetchEvent) => {
             }));
         }
 
-        sessions[session].postMessage({ type: 'GET_FILE_BLOB', filename}, [channel.port2]);
+        sessions[session].postMessage({ type: 'GET_FILE_BLOB', filename }, [channel.port2]);
     }))
 
 
-})
-
-//
-// self.addEventListener('fetch', (fetchEvent) => {
-//     let url = new URL(fetchEvent.request.url);
-//     let filename = url.pathname;
-//
-//     if (!filename.startsWith('/_/')) {
-//         fetchEvent.respondWith(fetch(fetchEvent.request));
-//         return
-//     }
-//
-//     let uuid = url.pathname.split('/_/')[1].split('/')[0];
-//     let ext = filename.split('.').pop();
-//
-//     fetchEvent.respondWith(
-//         sendMessage(uuidMap[uuid], {
-//             type: 'LOAD_FILE',
-//             name: filename
-//         })
-//         .then((data) => {
-//             console.log(data);
-//             let mimetype = extmap[ext];
-//             // if (!mimetype) {
-//             //     throw Error('No mapping to MIME-type for extension ' + ext);
-//             // }
-//             return new Response(data, {
-//                 headers: new Headers({ 'Content-Type':  'text/html'})
-//             });
-//         })
-//         // .catch(_ => fetch(fetchEvent.request)) // doomed to fail
-//     );
-// });
-//
-// self.addEventListener('message', (messageEvent) => {
-//     let message = messageEvent.data;
-//     switch (message.type) {
-//         case 'REGISTER_ZIP':
-//             // TODO handle multiple windows to same artifact better
-//             uuidMap[message.uuid] = messageEvent.ports[0];
-//             sendMessage(uuidMap[message.uuid], {
-//                 type: 'REGISTER_ZIP',
-//                 success: true
-//             });
-//             break;
-//         default:
-//             console.log("Unkown message type: " + message.type)
-//             break;
-//     }
-// })
+});
