1b0aeb8b72a9336abbb3c565ebbb3a3991a0334b
Evan Bolyen <ebolyen@gmail.com>
2017-03-07 20:26:41 -0700

    ENH: remove heartbeat hack (#43)

--- app/service-worker.js
+++ app/service-worker.js
@@ -6,25 +6,12 @@
 // The full license is in the file LICENSE, distributed with this software.
 // ----------------------------------------------------------------------------
 
-const sessions = {};
-
 self.addEventListener('install', (event) => {
     event.waitUntil(self.skipWaiting()); // Activate worker immediately
 });
 
 self.addEventListener('activate', (event) => {
     event.waitUntil(self.clients.claim()); // Become available to all pages
-});
-
-self.addEventListener('message', (event) => {
-    switch (event.data.type) {
-    case 'NEW_DOCUMENT':
-        sessions[event.data.session] = event.ports[0];
-        event.ports[0].postMessage(event.data);
-        break;
-    default:
-        return;
-    }
 });
 
 self.addEventListener('fetch', (fetchEvent) => {
@@ -36,14 +23,16 @@ self.addEventListener('fetch', (fetchEvent) => {
 
     const components = url.pathname.split('/').slice(2);  // discard '' and '_'
     const session = components[0];
+    const uuid = components[1];
     const filename = components.slice(2).join('/');  // everything but session/uuid
 
     fetchEvent.respondWith(new Promise((resolve, reject) => { // eslint-disable-line no-unused-vars
-        const channel = new MessageChannel();
-        channel.port1.onmessage = (event) => {
-            resolve(new Response(event.data));
-        };
+        self.clients.matchAll().then(clients => clients.forEach((client) => {
+            const channel = new MessageChannel();
+            channel.port1.onmessage = event => resolve(new Response(event.data));
 
-        sessions[session].postMessage({ type: 'GET_BLOB', filename }, [channel.port2]);
+            client.postMessage({ type: 'GET_BLOB', session, uuid, filename },
+                               [channel.port2]);
+        }));
     }));
 });
